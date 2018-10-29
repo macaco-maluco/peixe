50856843a0c69fa611ed861c6e26be7f24fe6124
Paulo Ragonha <paulo@ragonha.me>
2018-10-29 19:57:54 +0100

    First release! 🤞

---
+++ precache-manifest.d86ef130c283b51a8e4202919087e8f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "842917456787cc01e962",
+    "url": "/static/js/main.84291745.chunk.js"
+  },
+  {
+    "revision": "6d8731e4a294b495b482",
+    "url": "/static/js/1.6d8731e4.chunk.js"
+  },
+  {
+    "revision": "842917456787cc01e962",
+    "url": "/static/css/main.66381113.chunk.css"
+  },
+  {
+    "revision": "69885e41cf1d261daab933e6cd58c588",
+    "url": "/index.html"
+  }
+];
